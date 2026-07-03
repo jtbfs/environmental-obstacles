4a16a42b739e70eab86985fe4c89c86c70c8da2a
jtbfs <59122517+jtbfs@users.noreply.github.com>
2026-07-03 13:56:23 +0300

    Create Cache.cs
    responsible for caching all reflections

---
+++ Cache.cs
@@ -0,0 +1,27 @@
+using HarmonyLib;
+using SimulationScripts.BibiteScripts;
+using UnityEngine;
+
+namespace Bibites_Predatory_Obstacle
+{
+    internal class Cache
+    {
+        public static readonly AccessTools.FieldRef<FieldOfView, int> nBibitesInRange =
+            AccessTools.FieldRefAccess<FieldOfView, int>("nBibitesInRange");
+
+        public static readonly AccessTools.FieldRef<FieldOfView, int> nPlantsInRange =
+            AccessTools.FieldRefAccess<FieldOfView, int>("nPlantsInRange");
+
+        public static readonly AccessTools.FieldRef<FieldOfView, int> nMeatsInRange =
+            AccessTools.FieldRefAccess<FieldOfView, int>("nMeatsInRange");
+
+        public static readonly AccessTools.FieldRef<FieldOfView, int> nCorpsesInRange =
+            AccessTools.FieldRefAccess<FieldOfView, int>("nCorpsesInRange");
+
+        public static readonly AccessTools.FieldRef<BibiteBody, Rigidbody2D> rb2d =
+            AccessTools.FieldRefAccess<BibiteBody, Rigidbody2D>("rb2d");
+
+        public static readonly AccessTools.FieldRef<BibiteOrgan, BibiteGenes> genes =
+            AccessTools.FieldRefAccess<BibiteOrgan, BibiteGenes>("genes");
+    }
+}
